09a5845592d4c4c7d6c92cd85fa38e07cd1e6c3c
Felipe <fgoesoliveira@gmail.com>
2020-09-04 08:23:27 -0300

    fix: migrations

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vehicles.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cars_info.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
